7f795b9f1d60b1dd72321bdcf96c02bdbfd8636a
选择 <1725449934@qq.com>
2021-08-10 17:45:31 +0800

    优化

--- src/views/Bus.vue
+++ src/views/Bus.vue
@@ -332,7 +332,7 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #MAps {
   width: 100%;
   height: 100%;
--- src/views/RoadNetwork.vue
+++ src/views/RoadNetwork.vue
@@ -5,53 +5,92 @@
       <el-checkbox v-model="checked1" label="路网" border></el-checkbox>
       <el-checkbox v-model="checked2" label="卫星" border></el-checkbox>
     </el-checkbox-group> -->
-    <div class="checkgroup">
+    <!-- <div class="checkgroup">
       <el-checkbox v-model="RoadNet" label="路网" @change="RoadNet ? map.add(roadNetLayer) : map.remove(roadNetLayer)" border></el-checkbox>
       <el-checkbox v-model="Satellite" label="卫星" @change="Satellite ? map.add(satelliteLayer) : map.remove(satelliteLayer)" border></el-checkbox>
       <el-checkbox v-model="Traffic" label="实时路况" @change="Traffic ? map.add(TrafficLayer) : map.remove(TrafficLayer)" border></el-checkbox>
       <el-checkbox v-model="Buildings" label="楼快" @change="Buildings ? map.add(BuildingsLayer) : map.remove(BuildingsLayer)" border></el-checkbox>
-    </div>
+    </div> -->
     <div class="checkgroup">
-      <div class="check"></div>
-      <!-- <el-dropdown trigger="click">
-        <span class="el-dropdown-link"
-          ><i class="el-icon-arrow-down el-icon--right"></i> 下拉菜单<i class="el-icon-arrow-down el-icon--right"></i>
-          <el-switch v-model="value2" disabled> </el-switch>开关
-        </span>
-        <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item icon="el-icon-plus"
-            >黄金糕
-            <div><el-switch v-model="value2" active-text="ON" inactive-text="OFF"> </el-switch></div
-          ></el-dropdown-item>
-          <el-dropdown-item icon="el-icon-circle-plus"
-            >狮子头
-            <div><el-switch v-model="value2"> </el-switch></div
-          ></el-dropdown-item>
-          <el-dropdown-item icon="el-icon-circle-plus-outline"
-            >螺蛳粉
-            <div><el-switch v-model="value2"> </el-switch></div
-          ></el-dropdown-item>
-          <el-dropdown-item icon="el-icon-check"
-            >双皮奶
-            <div><el-switch v-model="value2"> </el-switch></div
-          ></el-dropdown-item>
-          <el-dropdown-item icon="el-icon-circle-check"
-            >蚵仔煎
-            <div><el-switch v-model="value2"> </el-switch></div
-          ></el-dropdown-item>
-        </el-dropdown-menu>
-      </el-dropdown> -->
+      <div class="check-box" @mouseover="selectStyle" @mouseout="outStyle">
+        <span class="check-left"><i class="iconfont icon-tuceng"></i>图层<i :class="isShow ? 'el-icon-arrow-down' : 'el-icon-arrow-up'"></i></span
+        ><span class="check-right">ON - OFF</span>
+        <ul v-show="isShow">
+          <li>
+            <div class="ifont"><i class="iconfont icon-ziyuan2"></i>&ensp;路网</div>
+            <el-switch
+              v-model="RoadNet"
+              active-text="ON"
+              inactive-text="OFF"
+              :width="60"
+              inactive-color="#8CC5FF"
+              @change="RoadNet ? map.add(roadNetLayer) : map.remove(roadNetLayer)"
+            >
+            </el-switch>
+          </li>
+          <li>
+            <div class="ifont"><i class="iconfont icon-weixing"></i>&ensp;卫星</div>
+            <el-switch
+              v-model="Satellite"
+              active-text="ON"
+              inactive-text="OFF"
+              :width="60"
+              inactive-color="#8CC5FF"
+              @change="Satellite ? map.add(satelliteLayer) : map.remove(satelliteLayer)"
+            >
+            </el-switch>
+          </li>
+          <li>
+            <div class="ifont"><i class="iconfont icon-shishilukuang"></i>&ensp;实时路况</div>
+            <el-switch
+              v-model="Traffic"
+              active-text="ON"
+              inactive-text="OFF"
+              :width="60"
+              inactive-color="#8CC5FF"
+              @change="Traffic ? map.add(TrafficLayer) : map.remove(TrafficLayer)"
+            >
+            </el-switch>
+          </li>
+          <li>
+            <div class="ifont"><i class="iconfont icon-jianzhu"></i>&ensp;楼快</div>
+            <el-switch
+              v-model="Buildings"
+              active-text="ON"
+              inactive-text="OFF"
+              :width="60"
+              inactive-color="#8CC5FF"
+              @change="Buildings ? map.add(BuildingsLayer) : map.remove(BuildingsLayer)"
+            >
+            </el-switch>
+          </li>
+          <li>
+            <div class="ifont"><i class="iconfont icon-relitu"></i>&ensp;热力图</div>
+            <el-switch
+              v-model="heatMaps"
+              active-text="ON"
+              inactive-text="OFF"
+              :width="60"
+              inactive-color="#8CC5FF"
+              @change="heatMaps ? heatmap.show() : heatmap.hide()"
+            >
+            </el-switch>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import wcJson from "../json/response_1628238808198.json";
+import heatmapData from "../json/heatMap.json";
 export default {
   name: "RoadNetwork",
   data() {
     return {
       map: {},
+      circles: [],
       markers: [],
       cluster: [],
       wcJsonData: [],
@@ -59,6 +98,9 @@ export default {
       Satellite: false,
       Traffic: false,
       Buildings: false,
+      isShow: false,
+      heatMaps: false,
+      heatmap: null,
       layers: [],
       roadNetLayer: new AMap.TileLayer.RoadNet(),
       satelliteLayer: new AMap.TileLayer.Satellite(),
@@ -83,7 +125,16 @@ export default {
     },
   },
   methods: {
+    selectStyle() {
+      this.isShow = true;
+    },
+    outStyle() {
+      this.isShow = false;
+    },
     initMaps() {
+      if (!this.isSupportCanvas()) {
+        alert("热力图仅对支持canvas的浏览器适用,您所使用的浏览器不能使用热力图功能,请换个浏览器试试~");
+      }
       // 配置地图的基本显示
       let that = this;
       that.map = new AMap.Map("MAps", {
@@ -104,11 +155,6 @@ export default {
       }
       console.log(wcDisabledData);
       for (let i = 0; i < this.wcJsonData.length; i++) {
-        // if (condition) {
-
-        // } else {
-
-        // }
         let WCMarker = new AMap.Marker({
           position: new AMap.LngLat(wcJsonData[i].location.split(",")[0], wcJsonData[i].location.split(",")[1]),
           // 将一张图片的地址设置为 icon
@@ -136,11 +182,13 @@ export default {
         this.markers[wcDisabledData[index]].setIcon(
           new AMap.Icon({
             image: this.WCdisabled,
-            size: new AMap.Size(70, 70),
-            imageSize: new AMap.Size(70, 70),
+            size: new AMap.Size(48, 48),
+            imageSize: new AMap.Size(48, 48),
           })
         );
+        this.circleMark(wcJsonData[wcDisabledData[index]].location);
       }
+
       //行政区修改样式
       let disProvince = new AMap.DistrictLayer.Province({
         zIndex: 12,
@@ -156,8 +204,94 @@ export default {
           "county-stroke": "rgba(255,255,255,0.5)", // 中国区县边界
         },
       });
+      //半径、层数、中心点、{填充颜色、初始透明度}
+      // that.CircleShow(600, 5, { fillColor: "#FF4D50", fillOpacity: 0.5 });
+      //参数：每一层播放的间隔时间、每一层扩散至最大所花费的总时间。
+      //循环每一层执行动画
+      // for (let [index, circle] of this.circles) {
+      //   animateStart(new Date().getTime() + index * distance, circle, index);
+      // }
       disProvince.setMap(this.map);
       // this.map.setFitView();
+    },
+    createHeatMap() {
+      if (!this.isSupportCanvas()) {
+        return this.$msg.error("热力图仅对支持canvas的浏览器适用,您所使用的浏览器不能使用热力图功能,请换个浏览器试试。");
+      }
+      let __this = this;
+      this.map.plugin(["AMap.Heatmap"], function () {
+        //初始化heatmap对象
+        __this.heatmap = new AMap.Heatmap(__this.map, {
+          visible: true,
+          radius: 25, //给定半径
+          opacity: [0, 0.8],
+        });
+        //设置数据集：该数据为北京部分“公园”数据
+        __this.heatmap.setDataSet({
+          data: heatmapData,
+          max: 100,
+        });
+      });
+      __this.heatmap.hide();
+    },
+    //圆形标记
+    circleMark(location) {
+      let circleNum = 0;
+      let circleNumTwo = 0;
+      let circleNumThree = 0;
+      let cricleArr = [];
+      for (let i = 0; i < 3; i++) {
+        let circleMarker = new AMap.Circle({
+          center: new AMap.LngLat(location.split(",")[0], location.split(",")[1]),
+          radius: 600, //半径
+          borderWeight: 3,
+          strokeColor: "#FF4D50",
+          strokeOpacity: 0.2,
+          strokeWeight: 1,
+          strokeOpacity: 0.2,
+          fillOpacity: 0.2,
+          strokeStyle: "dashed",
+          // strokeDasharray: [10, 10],
+          // 线样式还支持 'dashed'
+          fillColor: "#FF4D50",
+          zIndex: 50,
+        });
+        cricleArr.push(circleMarker);
+        cricleArr.forEach((item) => {
+          this.map.add(item);
+          // console.log(item);
+        });
+        this.CircleNumId = setInterval(() => {
+          cricleArr[0].setRadius((circleNum += 50));
+          if (circleNum > 2000) {
+            circleNum = 0;
+          }
+        }, 400);
+        setTimeout(() => {
+          this.CircleNumIdTwo = setInterval(() => {
+            cricleArr[1].setRadius((circleNumTwo += 50));
+            if (circleNumTwo > 2000) {
+              circleNumTwo = 0;
+            }
+          }, 400);
+        }, 1200);
+        setTimeout(() => {
+          this.CircleNumIdThree = setInterval(() => {
+            cricleArr[2].setRadius((circleNumThree += 50));
+            if (circleNumThree > 2000) {
+              circleNumThree = 0;
+            }
+          }, 400);
+        }, 2400);
+        // circleMarker.setMap(this.map)
+        // 缩放地图到合适的视野级别
+        // this.map.setFitView([ circleMarker ])
+      }
+    },
+    //判断浏览区是否支持canvas
+    isSupportCanvas() {
+      var elem = document.createElement("canvas");
+      return !!(elem.getContext && elem.getContext("2d"));
     },
     //行政区颜色
     getColorByAdcode(adcode) {
@@ -197,6 +331,7 @@ export default {
   async mounted() {
     await this.initMaps();
     this.addPoint();
+    this.createHeatMap();
   },
 };
 </script>
@@ -211,5 +346,101 @@ export default {
   left: 0;
   top: 0;
   background-color: #fff;
+  .check-box {
+    position: relative;
+    width: 200px;
+    height: 34px;
+    border-radius: 5px;
+    background-color: #333;
+    color: #fff;
+    span {
+      display: inline-block;
+      line-height: 24px;
+      font-size: 14px;
+      cursor: pointer;
+      text-align: center;
+    }
+    // .check-left::hover,
+    // .check-right::hover {
+    // }
+    .check-left {
+      width: 52%;
+      padding: 5px;
+      position: relative;
+      .icon-tuceng {
+        position: absolute;
+        left: 1rem;
+        top: 0.3rem;
+      }
+      .el-icon-arrow-down,
+      .el-icon-arrow-up {
+        position: absolute;
+        left: 5.5rem;
+        top: 0.6rem;
+      }
+    }
+    .check-left::after {
+      position: absolute;
+      top: 0.6em;
+      right: 0;
+      content: "";
+      width: 0;
+      height: 17px;
+      line-height: 24px;
+      border-left: solid rgb(224, 224, 230) 1px;
+    }
+    .check-right {
+      width: 37%;
+      padding: 5px;
+      // border-left: 2px solid #fff;
+    }
+    ul {
+      width: 100%;
+      list-style: none;
+      display: flex;
+      flex-direction: column;
+      position: absolute;
+      top: 31px;
+      left: 0;
+      li {
+        width: 190px;
+        height: 24px;
+        padding: 5px;
+        font-size: 14px;
+        line-height: 24px;
+        background-color: #333;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        .ifont {
+          width: 55%;
+          height: 100%;
+          line-height: 24px;
+        }
+        .el-switch {
+          width: 23%;
+          line-height: 24px;
+          margin-left: 30px;
+        }
+        /deep/.el-switch__label--right {
+          margin-left: -58px;
+          z-index: 125;
+          line-height: 22px;
+          font-size: 12px;
+          color: #fff !important;
+        }
+        /deep/.el-switch__label--left {
+          margin-right: -57px;
+          z-index: 125;
+          line-height: 22px;
+          font-size: 12px;
+          color: #fff !important;
+        }
+      }
+      li:last-child {
+        border-end-end-radius: 5px;
+      }
+    }
+  }
 }
 </style>
